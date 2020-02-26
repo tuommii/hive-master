f25156c039f3da8825bc54e607749f53b3d26a34
Willehard Korander <willehard@gmail.com>
2020-02-26 16:39:22 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hive Master
+<img src="https://github.com/wehard/hive-master/blob/master/assets/hive-master.png?raw=true"/>
--- main.go
+++ main.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	"github.com/wehard/ftapi"
 	"github.com/wehard/hive-master/game"
 	"github.com/wehard/hive-master/ui"
 )
@@ -13,18 +9,18 @@ import (
 
 func main() {
 
-	clientCredentials := ftapi.Authorize()
-	game.AuthorizedClientCredentials = clientCredentials
-	fmt.Println("Welcome, ", ftapi.GetAuthorizedUserData(clientCredentials.AccessToken).Displayname)
+	//clientCredentials := ftapi.Authorize()
+	//game.AuthorizedClientCredentials = clientCredentials
+	//fmt.Println("Welcome, ", ftapi.GetAuthorizedUserData(clientCredentials.AccessToken).Displayname)
 
-	_, err := os.Stat("game/users.json")
-	if err != nil {
-		if os.IsNotExist(err) {
-			fmt.Println("users file does not exist!")
-			userData := ftapi.RequestAllCampusUsersData(ftapi.Hive, clientCredentials.AccessToken)
-			ftapi.SaveUserData("game/users.json", userData)
-		}
-	}
+	//_, err := os.Stat("game/users.json")
+	//if err != nil {
+	//	if os.IsNotExist(err) {
+	//		fmt.Println("users file does not exist!")
+	//		userData := ftapi.RequestAllCampusUsersData(ftapi.Hive, clientCredentials.AccessToken)
+	//		ftapi.SaveUserData("game/users.json", userData)
+	//	}
+	//}
 
 	ui := &ui.UI2d{}
 	game.Run(ui)
